4a3f8ba68a2410427e038fa7a10c70593095e0f8
Jamat Ali <engr.jamatali@gmail.com>
2024-01-28 20:39:43 +0500

    userNum

---
+++ userInput/userInput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int userNum = 15;
+    cout << userNum;
+    // cin >> userNum;
+    // cout << "You Have Entered" << userNum << "Number";
+    return 0;
+}
