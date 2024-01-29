2094f3b0ef404368da109d9dd0f40d20551e8756
Jamat Ali <engr.jamatali@gmail.com>
2024-01-29 10:25:41 +0500

    Sum of two numbers from user

---
+++ userSum/userSum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+    cout << "Enter Number and Calculate Sum \n";
+    cout << "Enter first Number:" << endl;
+    cin >> a;
+    cout << "Enter 2nd Number: \n ";
+    cin >> b;
+    sum = a + b;
+    cout << sum;
+    return 0;
+}
