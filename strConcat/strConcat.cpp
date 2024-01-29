ef88f1bac30e64deab739500393fa669ce244199
Jamat Ali <engr.jamatali@gmail.com>
2024-01-29 13:54:58 +0500

    Accessing string characters

--- strConcat/strConcat.cpp
+++ strConcat/strConcat.cpp
@@ -13,5 +13,12 @@ int main()
     cout << fullName << endl;
     cout << "The String Length of fName is: " << fName.length() << endl;
     cout << "The Length of fullName is: " << fullName.size() << endl;
+    // String Char Accessing:
+
+    cout << fName[0] << endl;
+    cout << fName[1] << endl;
+    cout << fName[2] << endl;
+    cout << fName[3] << endl;
+    cout << fName[4] << endl;
     return 0;
 }
