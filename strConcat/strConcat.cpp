cfddd12c1f0a1615c6c00008f4445c0231096714
Jamat Ali <engr.jamatali@gmail.com>
2024-01-29 12:44:06 +0500

    string concatenation

---
+++ strConcat/strConcat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string fName = "Jamat";
+    string lName = "Ali";
+
+    cout << fName + " " + lName;
+
+    return 0;
+}
