b2db0dd52997f1fdf76f4839f8dd25d4e2e9a109
Jamat Ali <engr.jamatali@gmail.com>
2024-01-29 13:57:33 +0500

    changing string character

--- strConcat/strConcat.cpp
+++ strConcat/strConcat.cpp
@@ -15,10 +15,14 @@ int main()
     cout << "The Length of fullName is: " << fullName.size() << endl;
     // String Char Accessing:
 
-    cout << fName[0] << endl;
-    cout << fName[1] << endl;
-    cout << fName[2] << endl;
-    cout << fName[3] << endl;
-    cout << fName[4] << endl;
+    // cout << fName[0] << endl;
+    // cout << fName[1] << endl;
+    // cout << fName[2] << endl;
+    // cout << fName[3] << endl;
+    // cout << fName[4] << endl;
+
+    fullName[0] = 't';
+    cout << fullName;
+
     return 0;
 }
