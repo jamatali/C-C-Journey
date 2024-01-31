4a8f7b59b8909e58b381696222913c26bceea67a
Jamat Ali <engr.jamatali@gmail.com>
2024-01-31 23:41:09 +0500

    bool

--- boolCheck/boolCheck.cpp
+++ boolCheck/boolCheck.cpp
@@ -7,6 +7,11 @@ int main()
 {
     int x = 4;
     int y = 6;
-    cout << (x > y ? x : y);
+    cout << (x > y ? x : y) << endl;
+
+    bool isOk = true;
+    bool isDone = false;
+    cout << isOk << endl;
+    cout << isDone << endl;
     return 0;
 }
