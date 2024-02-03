b9e6d5220a7527092d66de919d760a60dc527d91
Jamat Ali <engr.jamatali@gmail.com>
2024-02-03 12:29:11 +0500

    usage of break statement

--- for/for.cpp
+++ for/for.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main()
 {
-    for (int i = 0; i < 5; i++)
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << i << "\n";
+    // }
+    // for (int i = 0; i <= 10; i = i + 2)
+    // {
+    //     cout << i << "\n";
+    // }
+    for (int i = 0; i < 10; i++)
     {
-        cout << i << "\n";
-    }
-    for (int i = 0; i <= 10; i = i + 2)
-    {
+        if (i == 4)
+        {
+            break;
+        }
         cout << i << "\n";
     }
     return 0;
