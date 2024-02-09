530bc5e73d98569589950b9fd0d943ffbb8c0bcb
Jamat Ali <engr.jamatali@gmail.com>
2024-02-09 23:51:36 +0500

    if else for

--- HelloWorld/helloWorld.cpp
+++ HelloWorld/helloWorld.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World"; // Just Printing
+    cout << "Hello World" << endl; // Just Printing
+    for (int i = 0; i <= 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << ": Hello Evenly" << endl;
+        }
+        else
+        {
+            cout << "Hello Odd" << endl;
+        }
+    }
     return 0;
 }
