b318e72b34013f6a6fefd3823ebd34353621aa8c
Jamat Ali <engr.jamatali@gmail.com>
2024-02-09 23:58:09 +0500

    multi string

--- Struct/multiStruct.cpp
+++ Struct/multiStruct.cpp
@@ -2,28 +2,33 @@
 #include <string>
 using namespace std;
 
-
-struct car {
+struct car
+{
   string brand;
   string model;
   int year;
 };
 
-int main() {
+int main()
+{
   car myCar1;
   myCar1.brand = "BMW";
   myCar1.model = "X5";
   myCar1.year = 1999;
 
- 
   car myCar2;
   myCar2.brand = "Ford";
   myCar2.model = "Mustang";
   myCar2.year = 1969;
- 
+
+  car myCar3;
+  myCar3.brand = "Ferrari";
+  myCar3.model = "Nawabi";
+  myCar3.year = 1980;
 
   cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
   cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
- 
+  cout << myCar3.brand << " " << myCar3.model << " " << myCar3.year << "\n";
+
   return 0;
 }
