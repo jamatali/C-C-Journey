50942c5179be6de25d5f11889b7927c213401572
unknown <engr.jamatali@gmail.com>
2024-03-06 16:16:25 +0500

    pointers

--- reference/reference.cpp
+++ reference/reference.cpp
@@ -6,9 +6,15 @@ int main()
 {
     string food = "Pizza";
     string &meal = food;
+    string* ptr = &food;
 
     cout << food << "\n";
     cout << meal << "\n";
-    cout << &food; //This will Give memory address of a variable
+    cout << food << endl; //This will print the value of food which is Pizza
+    cout << &food << endl; //This will Give memory address of a variable
+    cout << ptr << endl; //This will print the memory address as pointer
+
+
+
     return 0;
 }
