c4b47dee72e9cc4427ec2754fd882b502d3d20e8
unknown <engr.jamatali@gmail.com>
2024-03-07 23:17:34 +0500

    First C++ Function

---
+++ functions/function.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+void myFunction () {
+    cout << "This is My First C++ Function" <<endl;
+}
+int main () {
+    myFunction();
+    return 0;
+}
