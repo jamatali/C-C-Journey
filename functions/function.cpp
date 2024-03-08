ecaf94b427f9f1d3229f2a1f217fe599798a680d
unknown <engr.jamatali@gmail.com>
2024-03-08 17:47:19 +0500

    multiple arguments

--- functions/function.cpp
+++ functions/function.cpp
@@ -2,10 +2,27 @@
 #include <iostream>
 using namespace std;
 
-void myFunction (string x = "10") {
-    cout << "This is My First C++ Function" << " " << x <<endl;
+// void myFunction (string x = "10") {
+//     cout << "This is My First C++ Function" << " " << x <<endl;
+// }
+// int main () {
+//     myFunction("20");
+//     return 0;
+// }
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
+
+// int main() {
+//   cout << myFunction(5, 3);
+//   return 0;
+// }
+int myFunction(int x, int y) {
+  return x + y;
 }
-int main () {
-    myFunction("20");
-    return 0;
+
+int main() {
+  int z = myFunction(5, 3);
+  cout << z;
+  return 0;
 }
