a061b51bc9afdd8f94d48d9b1ca17249a68fca35
unknown <engr.jamatali@gmail.com>
2024-03-18 13:02:08 +0500

    class and objects, multi objects

--- classes/classes.cpp
+++ classes/classes.cpp
@@ -9,6 +9,13 @@ int myNum;      // Atrribute
 string myStr;   //Attribute
 };
 
+class Car {
+    public:
+    string brand;
+    string model;
+    int year;
+};
+
 int main(){
 
 myClass myObj; // create an object of my class
@@ -23,6 +30,18 @@ cout << "Int Attibute: " << myObj.myNum << endl;
 cout << "Str Attibute: " << myObj.myStr << endl;
 cout << "Int Attibute: " << x << endl;
 
+Car carObj1, carObj2;
+
+carObj1.brand = "Suzuki";
+carObj1.model = "Mehran-420";
+carObj1.year = 2020;
+
+carObj2.brand = "Atlas";
+carObj2.model = "CD-70-Delux";
+carObj2.year = 2015;
+
+cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year <<endl;
+cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year <<endl;
 
     return 0;
 };
