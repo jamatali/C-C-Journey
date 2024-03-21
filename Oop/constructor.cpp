f0cd8cad0a6e2c865ad5f5b5a657caa760ef7107
unknown <engr.jamatali@gmail.com>
2024-03-21 11:38:32 +0500

    constructor with parameters

--- Oop/constructor.cpp
+++ Oop/constructor.cpp
@@ -5,10 +5,28 @@ class conClass{
 public:
 conClass(){
     cout << "Constructor" <<endl;
+}
 };
+
+class conPara{
+public:
+string brand;
+string model;
+int year;
+conPara(string x, string y, int z){
+brand = x;
+model = y;
+year = z;
+}
 };
 int main (){
 conClass conObj;
+conPara obj1("BMW", "Sozo33", 1990), obj2("Suzuki", "Loso44", 2002);
+
+cout << obj1.brand << " " << obj1.model << " " << obj1.year <<endl;
+
+cout << obj2.brand << " " << obj2.model << " " << obj2.year <<endl;
+
     return 0;
 };
 
